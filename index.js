36c38d992e999ae796a810d3181208287cdf5084
Siddesh-shilkar <shilkarsiddesh@gmail.com>
2021-02-26 16:52:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
-const readline = require('readline')
 const fs = require('fs')
 
+let buffer = '';
 
-const rl = readline.createInterface({
-    input: fs.createReadStream('./test.json')
-    
-})
+const processObj = (obj) => {
+    try {
+        if (obj[obj.length -1] === '/r') {
+            obj = obj.substr(0, obj.length-1)
+            // console.log(obj)
+        }
+        if (obj.length > 0) {
+            let parsedObj = JSON.parse(JSON.stringify(obj));
+            // console.log(typeof parsedObj, 'typeof parsedObj')
+            let write = fs.createWriteStream('./writeTest.json', { 'flags': 'a', 'encoding': null, 'mode': 0666 })
+            write.write(parsedObj)
+        }
+    } catch (err) {
+        console.log(err)
+    }
+}
 
-rl.on('line', (data) => {
-    let writeData = JSON.parse(data)
-    let write = fs.createWriteStream('./writeTest.json', { 'flags': 'a', 'encoding': null, 'mode': 0666})
-    write.write(writeData + '\n')
+
+const processData = () => {
+    let stringPosition;
+
+    while((stringPosition = buffer.indexOf(`\n`)) >= 0) {
+        if (stringPosition === 0) {
+            buffer = buffer.slice(1);
+            continue
+        }
+
+        // let stringifiedJson = JSON.stringify(buffer);
+        // console.log(buffer.slice(0, stringPosition + 1), '<------- sliced buffer')
+        let slicedBuffer = buffer.slice(0, stringPosition + 1)
+        processObj(slicedBuffer)
+
+        // buffer = ''
+        buffer = buffer.slice(stringPosition+1)
+    }
+}
+
+
+let dataStream = fs.createReadStream('test.json', {flags: 'r', encoding: 'utf-8'})
+
+dataStream.on('data', (chunk) => {
+    buffer += chunk.toString()
+    processData() 
 })
